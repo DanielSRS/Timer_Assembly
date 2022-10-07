c07159fa6ecf8051259f0eaf592813364ffd2324
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2022-10-07 10:52:30 -0300

    📝 Document timer code

--- src/v3/Timer_v3_lib.s
+++ src/v3/Timer_v3_lib.s
@@ -1,11 +1,11 @@
-.equ pagelen, 4096
+.equ pagelen, 4096              @ Tamanho da pagina de memoria paginada
 .equ prot_read, 1
 .equ prot_write, 2
 .equ map_shared, 1
-.equ sys_open, 5
-.equ sys_map, 192
-.equ nano_sleep, 162
-.equ level, 52
+.equ sys_open, 5                @ Numero da syscall para abrir arquivo
+.equ sys_map, 192               @
+.equ nano_sleep, 162            @ Numero da syscall para nanosleep
+.equ level, 52                  @ Offset para o registrador de niveis da GPIO
 
 
 .global _start
@@ -17,11 +17,13 @@
     SVC 0
 .endm
 
+@ Limpa todo o conteúdo do display
 .macro ClearDisplay
     mov r0, r8
     bl clear_display
 .endm
 
+@ Escreve um digito (0-9) no display
 .macro writeDigit value
 
     mov r0, r8
@@ -30,6 +32,7 @@
 
 .endm
 
+@ Escreve um caractere no display
 .macro writeChar value
 
     mov r0, r8              @ primeiro argumento é o endereço base da GPIO
@@ -38,6 +41,8 @@
 
 .endm
 
+@ Mapeia o endereço do registrador base da GPIO
+@ Salva em r8 o endereço base da GPIO
 .macro MapAddressGPIO
 
     @ opening the file
@@ -63,6 +68,7 @@
 
 .endm
 
+@ Inicializa o display 16x2
 .macro InitDisplay
     mov r0, r8
     bl init_display
@@ -73,27 +79,29 @@ _start:
     MapAddressGPIO          @ Mapeia o endereço base da GPIO
     InitDisplay             @ Inicializa o display
     InitDisplay
-    MOV R12, #9
-    MOV R4, #9
+
+    @ Inicia o timer em 99 segundos
+    MOV R12, #9             @ Digito da dezena
+    MOV R4, #9              @ Digito da unidade
 
     @ Exibe uma mensagem inicial
     
-    writeChar #65  @A
-    writeChar #112 @p
-    writeChar #101 @e
-    writeChar #114 @r
-    writeChar #116 @t
-    writeChar #101 @e
-    writeChar #32  @
+    writeChar #65   @A
+    writeChar #112  @p
+    writeChar #101  @e
+    writeChar #114  @r
+    writeChar #116  @t
+    writeChar #101  @e
+    writeChar #32   @
 
-    writeChar #111 @o
-    writeChar #32  @
+    writeChar #111  @o
+    writeChar #32   @
 
-    writeChar #66  @B
-    writeChar #111 @o
-    writeChar #116 @t
-    writeChar #97 @ã
-    writeChar #111 @o
+    writeChar #66   @B
+    writeChar #111  @o
+    writeChar #116  @t
+    writeChar #97   @ã
+    writeChar #111  @o
 
 
 
@@ -136,49 +144,50 @@ wait_button_up_pause:
 
 
 loop:
-    ClearDisplay
-    writeDigit R12
-    writeDigit R4
+    ClearDisplay            @ Limpa o conteúdo do display
+    writeDigit R12          @ Escreve o digito de dezena
+    writeDigit R4           @ Escreve o digito de unidade
 
 
 
-    MOV R6, #13
-    verifica_button:
-        LDR R9, [R8, #level]
+    MOV R6, #13             @ Tempo passado em grupos de 100ms
+verifica_button:
+    LDR R9, [R8, #level]    @ Leitura dos niveis dos pinos da GPIO
 
-        MOV R11, #1
-        LSL R11, #5
-        MOV R5, #1
-        LSL R5, #26
+    MOV R11, #1             @ Mascara o bit referente ao
+    LSL R11, #5             @ botão de start/pause
 
-        AND R10, R5, R9
-        cmp R10, #0
-        beq restart
+    MOV R5, #1              @ Mascara o bit referente ao
+    LSL R5, #26             @ botão de restart
 
-        AND R10, R11, R9
-        CMP R10, #0
-        bleq wait_button_up_pause
+    AND R10, R5, R9         @ Descarta o valores não relevantes
+    cmp R10, #0             @ verifica se o nivel do botão de restart é 0 (pressionado)
+    beq restart             @ Se pressionado, reinicia timer
 
-        nanoSleep2 time100ms
-        SUB R6, R6, #1
-        CMP R6, #0
-        BNE verifica_button
+    AND R10, R11, R9        @ Descarta o valores não relevantes
+    CMP R10, #0             @ verifica se o nivel do botão de start/pause é 0 (pressionado)
+    bleq wait_button_up_pause   @ Se pressionado, pausa o timer
 
+    @ Se nenhum botão foi pressionado, continua contagem do timer
+    nanoSleep2 time100ms    @ Aguarda 100ms
+    SUB R6, R6, #1          @ Decrementa 100ms do tempo passado
+    CMP R6, #0              @ Verifica se já se passou 1 segundo (1000ms)
+    BNE verifica_button     @ Se ainda não passou, continua contagem
 
-
+    @ Se ja se passou 1 segundo, decrementa unidade de segundos do timer
     SUB R4, R4, #1
-    CMP R4, #0
+    CMP R4, #0              @ Verifica se a unidade de segundos foi zerada
 
-    BGE loop
+    BGE loop                @ Se falso volta para inicio do loop e atualiza
+                            @ as informações exibidas
 
+    SUB R12, R12, #1        @ Se verdadeiro, decrementa 1s do digito de dezena
+    CMP R12, #0             @ Verifica se contagem do timer terminou
 
-    SUB R12, R12, #1
-    CMP R12, #0
+    BLT restart             @ Se terminado, reinicia contador
 
-    BLT restart
-
-    MOV R4, #9
-    b loop
+    MOV R4, #9              @ Se não, reseta o digito de unidade para 9 segundos
+    b loop                  @ e continua contagem
 
 end:
     MOV R7, #1
@@ -188,8 +197,9 @@ end:
 .data
 
 fileName: .asciz "/dev/mem"
-gpioaddr: .word 0x20200
+gpioaddr: .word 0x20200            @ Endereço base (fisico) da GPIO
 
+@ Struct timespec argumento do nanosleep
 time100ms:
     .word 0
     .word 100000000
